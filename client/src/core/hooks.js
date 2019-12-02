12e4f85dcd0d4e55ef51d2069e56bdaf52f438e0
Steven Ortiz <sortiz832@gmail.com>
2019-12-01 18:38:41 -0600

    Updated hook

--- client/src/core/hooks.js
+++ client/src/core/hooks.js
@@ -4,16 +4,27 @@ import {React} from 'core/react';
 const _useEffect = React.useEffect;
 
 /**
- * Constructs the state on each update (without callbacks).
+ * Constructs the state once and mutates the state on subsequent updates.
  */
 export function useClassState(constructor, initial) {
-    return React.useReducer(
-        (state, update) => Object.assign(new constructor(), state, update),
+    const state = React.useRef(
         React.useMemo(
             () => Object.assign(new constructor(), initial),
             [constructor, initial],
-        ),
+        )
     );
+    const [_, setState] = React.useReducer(
+        (prevState, updater) => {
+            if(typeof updater !== 'function') {
+                Object.assign(state.current, updater);
+            } else {
+                updater(state.current);
+            }
+            return !prevState;
+        },
+        true,
+    );
+    return [state.current, setState];
 }
 
 /**
