4752c9279f992d48a0f4443ada7de32639f0dad7
Steven Ortiz <sortiz832@gmail.com>
2020-02-17 10:26:42 -0600

    Replaced class names

--- client/src/core/react.js
+++ client/src/core/react.js
@@ -1,6 +1,3 @@
+export {default as classNames} from 'classnames';
 export {default as React, Fragment} from 'react';
 export {default as ReactDom} from 'react-dom';
-
-export function classNames(...args) {
-    return args.filter(s => typeof s === 'string' && s.length > 0).join(' ');
-}
