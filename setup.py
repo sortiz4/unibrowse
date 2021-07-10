8212ccf535ae28948ddefffc57eb452f33afbbc6
Steven Ortiz <sortiz832@gmail.com>
2021-07-10 16:38:43 +0100

    Updated script

--- setup.py
+++ setup.py
@@ -9,17 +9,11 @@ CLIENT_DIR = os.path.join(BASE_DIR, 'client')
 CLIENT_ASSET_DIR = os.path.join(CLIENT_DIR, 'build')
 SERVER_DIR = os.path.join(BASE_DIR, 'server')
 SERVER_ASSET_DIR = os.path.join(SERVER_DIR, 'wwwroot')
-
-
-class Setup:
-    HELP = 'Download the client and server dependencies.'
-    CLIENT = ['npm', 'install']
-    SERVER = ['dotnet', 'restore']
-
-
-class Build:
-    HELP = 'Compile and move the client assets to the server.'
-    CLIENT = ['npm', 'run', 'build']
+SETUP_HELP = 'Download the client and server dependencies.'
+SETUP_CLIENT = ['npm', 'install']
+SETUP_SERVER = ['dotnet', 'restore']
+BUILD_HELP = 'Compile and move the client assets to the server.'
+BUILD_CLIENT = ['npm', 'run', 'build']
 
 
 class Command:
@@ -27,8 +21,8 @@ class Command:
 
     def __init__(self):
         parser = argparse.ArgumentParser(description=self.help)
-        parser.add_argument('-s', '--setup', action='store_true', help=Setup.HELP)
-        parser.add_argument('-b', '--build', action='store_true', help=Build.HELP)
+        parser.add_argument('-s', '--setup', action='store_true', help=SETUP_HELP)
+        parser.add_argument('-b', '--build', action='store_true', help=BUILD_HELP)
         self.args = parser.parse_args()
 
     def handle(self):
@@ -40,15 +34,15 @@ class Command:
     @classmethod
     def setup(cls):
         os.chdir(CLIENT_DIR)
-        run(Setup.CLIENT, shell=True)
+        run(SETUP_CLIENT, shell=True)
         os.chdir(SERVER_DIR)
-        run(Setup.SERVER, shell=True)
+        run(SETUP_SERVER, shell=True)
 
     @classmethod
     def build(cls):
         # Compile the client
         os.chdir(CLIENT_DIR)
-        run(Build.CLIENT, shell=True)
+        run(BUILD_CLIENT, shell=True)
 
         # Move the assets to the server
         dir_util.copy_tree(CLIENT_ASSET_DIR, SERVER_ASSET_DIR)
